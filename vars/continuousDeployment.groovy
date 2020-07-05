475d6cf4eef0d4da9a250edfdf596290b0f998ed
Hugues McNeil-Duval <huguesmcd@MacBook-Pro-de-Hugues.local>
2020-07-05 16:07:14 -0400

    Continue...

--- vars/continuousDeployment.groovy
+++ vars/continuousDeployment.groovy
@@ -14,7 +14,7 @@ def call() {
     ProcessBranchNameRequest processBranchNameRequest = new ProcessBranchNameRequest(BRANCH_NAME)
     ProcessBranchNameResponse processBranchNameResponse = processBranchName(processBranchNameRequest)
 
-    if ( processBranchNameResponse.doBuild ) {
+    if ( false && processBranchNameResponse.doBuild ) {
       echo "### Building app on version '${processBranchNameResponse.version}'"
       BuildRequest buildRequest = new BuildRequest(conf, processBranchNameResponse.version)
       build(buildRequest)
